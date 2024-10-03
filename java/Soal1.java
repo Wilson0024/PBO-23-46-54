7413fbfde10fa551316760b8078ff5d744f933aa
Wilson0024 <wilsonangelietann@gmail.com>
2024-10-03 19:15:00 +0700

    up tugas

--- java/Soal1.java
+++ java/Soal1.java
@@ -1,27 +1,19 @@
-/*
- * Anggota Kelompok :
- * - Wilson Angelie Tan (140810230024)
- * - Stan Fedheric (140810230046)
- * - Theopillus Samuel Ghozalli (140810230054)
- * 
- * Deskripsi : Soal 1 dalam Bahasa Pemograman Java
- */
 import java.util.Scanner;
-class Waktu{
+
+class UjianTPB{
     static Scanner sc = new Scanner(System.in);
 
     private String nama;
     private String npm;
-    private int jam;
-    private int menit;
-    private int detik;
+    private Waktu mulai;
+    private Waktu selesai;
 
-    public Waktu(){
+
+    public UjianTPB(){
         this.nama = "";
         this.npm = "";
-        this.jam = 0;
-        this.menit = 0;
-        this.detik = 0;
+        this.mulai = new Waktu();
+        this.selesai = new Waktu();
     }
 
     public void setNama(){
@@ -29,9 +21,99 @@ class Waktu{
         this.nama = sc.nextLine();
     }
 
+    public void setMulai() {
+        System.out.println("\n\n\tJam Mulai");
+        System.out.println("=======================");
+
+        // set jam menit detik mulai
+        mulai.setJam(); 
+        mulai.setMenit();
+        mulai.setDetik();
+    }
+
+    public void setSelesai() {
+        System.out.println("\n\n\tJam Selesai");
+        System.out.println("=======================");
+
+        // set jam menit detik selesai
+        selesai.setJam();
+        selesai.setMenit();
+        selesai.setDetik();
+    }
+
     public void setNpm(){
         System.out.print("Masukkan NPM : ");
         this.npm = sc.nextLine();
+    }
+
+    public String getNama(){
+        return this.nama;
+    }
+
+    public String getNpm(){
+        return this.npm;
+    }  
+
+    public Waktu getMulai() {
+        return mulai;
+    }
+
+    public Waktu getSelesai() {
+        return selesai;
+    }
+
+    public char HurufMutu(float lamaLari){
+        if(lamaLari >= 0 && lamaLari < 7.5){
+            return 'A';
+        }
+        else if(lamaLari >= 7.5 && lamaLari < 12.5){
+            return 'B';
+        }
+        else if(lamaLari >= 12.5 && lamaLari < 30){
+            return 'C';
+        }
+        else{
+            return 'D';
+        }
+    }
+
+    public String status(char HurufMutu){
+        if(HurufMutu == 'A' || HurufMutu == 'B' || HurufMutu == 'C'){
+            return "Lulus";
+        }
+        else{
+            return "Gagal";
+        }
+    }
+
+        // Tampilkan  Nama, NPM, HM, status lulus, waktu mulai, waktu selesai dan lama lari
+    public void tampilan(){
+        Waktu selisih = new Waktu();// nyimpen lama waktu lari
+        selisih.selisihWaktu(this.mulai, this.selesai);
+
+        System.out.println("\n\n\tOUTPUT");
+        System.out.println("===========================");
+        System.out.println("Nama : " + this.nama);
+        System.out.println("NPM : " + this.npm);
+        System.out.println("Huruf Mutu : " + HurufMutu(selisih.lamaLari()));
+        System.out.println("Status : " + status(HurufMutu(selisih.lamaLari())));
+        System.out.println("Waktu Mulai : " + mulai.getJam() + ":" + mulai.getMenit() + ":" + mulai.getDetik());
+        System.out.println("Waktu Selesai : " + selesai.getJam() + ":" + selesai.getMenit() + ":" + selesai.getDetik());
+        System.out.println("Lama Lari : " + selisih.lamaLari() + " Menit\n\n");
+    }
+}
+
+class Waktu{
+    static Scanner sc = new Scanner(System.in);
+
+    private int jam;
+    private int menit;
+    private int detik;
+
+    public Waktu(){
+        this.jam = 0;
+        this.menit = 0;
+        this.detik = 0;
     }
 
     public void setJam(){
@@ -40,7 +122,6 @@ class Waktu{
 
     public void setMenit(){
         this.menit = inputBatas("Masukkan Menit (0-59): ", 0, 59);
-
     }
 
     public void setDetik(){
@@ -69,81 +150,46 @@ class Waktu{
         return this.menit;
     }
 
-    public String getNama(){
-        return this.nama;
-    }
-
-    public String getNpm(){
-        return this.npm;
-    }
-
-    public float lamaLari(Waktu selesai){
-        float ja = selesai.jam - this.jam;
-        ja = (ja < 0)? ja+24 : ja;
-        float totalMenit = ja*60 + (selesai.menit - this.menit) + (float)(selesai.detik - this.detik)/60;
-        return totalMenit;
-    }
-
-    public char HurufMutu(float lamaLari){
-        if(lamaLari >= 0 && lamaLari < 7.5){
-            return 'A';
-        }
-        else if(lamaLari >= 7.5 && lamaLari < 12.5){
-            return 'B';
-        }
-        else if(lamaLari >= 12.5 && lamaLari < 30){
-            return 'C';
+    public void selisihWaktu(Waktu mulai, Waktu selesai){
+        if(selesai.detik - mulai.detik < 0){
+            this.menit -= 1;
+            this.detik = selesai.detik - mulai.detik + 60;
         }
         else{
-            return 'D';
+            this.detik = selesai.detik - mulai.detik;
         }
-    }
-
-    public String status(char HurufMutu){
-        if(HurufMutu == 'A' || HurufMutu == 'B' || HurufMutu == 'C'){
-            return "Lulus";
+        
+        if(selesai.menit - mulai.menit < 0){
+            this.jam -= 1;
+            this.menit += selesai.menit - mulai.menit + 60;
         }
         else{
-            return "Gagal";
+            this.menit += selesai.menit - mulai.menit;
+        }
+
+        this.jam += selesai.jam - mulai.jam;
+        
+        if(this.jam < 0){
+            this.jam += 24;
         }
     }
 
-    // Tampilkan  Nama, NPM, HM, status lulus, waktu mulai, waktu selesai dan lama lari
-    public void tampilan(Waktu selesai){
-        System.out.println("\n\nNama : " + this.nama);
-        System.out.println("NPM : " + this.npm);
-        System.out.println("Huruf Mutu : " + HurufMutu(lamaLari(selesai)));
-        System.out.println("Status : " + status(HurufMutu(lamaLari(selesai))));
-        System.out.println("Waktu Mulai : " + this.jam + ":" + this.menit + ":" + this.detik);
-        System.out.println("Waktu Selesai : " + selesai.jam + ":" + selesai.menit + ":" + selesai.detik);
-        System.out.println("Lama Lari : " + lamaLari(selesai) + " Menit\n\n");
-        sc.close();
+    public float lamaLari(){
+        return (this.jam * 60) + this.menit + (float)this.detik/60;
     }
 }
 
 public class Soal1 {
+    static void Menu(){
+        UjianTPB dataMhs = new UjianTPB();
+        
+        dataMhs.setNama(); //set nama
+        dataMhs.setNpm();  // set npm
+        dataMhs.setMulai();
+        dataMhs.setSelesai();
+        dataMhs.tampilan();
+    }
     public static void main(String[] args) {
-        Waktu dataMhs = new Waktu();
-        Waktu selesai = new Waktu();
-
-        dataMhs.setNama();
-        dataMhs.setNpm();
-        
-        System.out.println("\n\n\tJam Mulai");
-        System.out.println("=======================");
-
-        dataMhs.setJam();
-        dataMhs.setMenit();
-        dataMhs.setDetik();
-
-
-        System.out.println("\n\n\tJam Selesai");
-        System.out.println("=======================");
-
-        selesai.setJam();
-        selesai.setMenit();
-        selesai.setDetik();
-        
-        dataMhs.tampilan(selesai);
+        Menu();
     }
 }
--- python/Soal1.py
+++ python/Soal1.py
@@ -1,106 +1,124 @@
- # Anggota Kelompok :
- # - Wilson Angelie Tan (140810230024)
- # - Stan Fedheric (140810230046)
- # - Theopillus Samuel Ghozalli (140810230054)
- # 
- # Deskripsi : Soal 1 dalam Bahasa Pemograman Python
- #
-class Waktu :
+class Waktu:
     def __init__(self):
-        self.__nama = ""
-        self.__npm = ""
         self.__jam = 0
         self.__menit = 0
         self.__detik = 0
-        
-    def inputBatas(self,kata, min, max):
-        data = -1
-        while data < min or data > max :
-            data = int(input(kata))
-        return data
-    
-    def setNama(self):
-        self.__nama = str(input("Masukkan Nama :"))
-    
-    def setNpm(self):
-        self.__npm = str(input("Masukkan NPM : "))
-    
-    def setJam(self):
-        self.__jam = self.inputBatas("Masukkan Jam : ", 0, 23)
-    
-    def setMenit(self):
-        self.__menit = self.inputBatas("Masukkan Menit : ", 0, 59)
-    
-    def setDetik(self):
-        self.__detik = self.inputBatas("Masukkan Detik : ", 0, 59)
-        
-    def getDetik(self):
-        return self.__detik
 
+    def setJam(self):
+        self.__jam = self.inputBatas("Masukkan Jam (0-23): ", 0, 23)
+
+    def setMenit(self):
+        self.__menit = self.inputBatas("Masukkan Menit (0-59): ", 0, 59)
+
+    def setDetik(self):
+        self.__detik = self.inputBatas("Masukkan Detik (0-59): ", 0, 59)
+    
     def getJam(self):
         return self.__jam
-
+    
     def getMenit(self):
         return self.__menit
-
-    def getNama(self):
-        return self.__nama
-
-    def getNpm(self):
-        return self.__npm
-   
-    def lamaLari(self, selesai):
-        ja = selesai.__jam - self.__jam
-        if ja < 0 :
-            ja = ja+24
-        return ja*60 + (selesai.__menit - self.__menit) + (selesai.__detik - self.__detik)/60.0
     
-    def hurufMutu(self, lamaLari):
-        if lamaLari >= 0 and lamaLari < 7.5 :
+    def getDetik(self):
+        return self.__detik
+    
+    def inputBatas(self, kata, min_val, max_val):
+        while True:
+                data = int(input(kata))
+                if min_val <= data <= max_val:
+                    return data
+                else:
+                    print(f"Input harus antara {min_val} dan {max_val}")
+
+    def selisihWaktu(self, mulai, selesai):
+        self.__detik = selesai.__detik - mulai.__detik
+        self.__menit = selesai.__menit - mulai.__menit
+        self.__jam = selesai.__jam - mulai.__jam
+        
+        if self.__detik < 0:
+            self.__detik += 60
+            self.__menit -= 1
+        if self.__menit < 0:
+            self.__menit += 60
+            self.__jam -= 1
+        if self.__jam < 0:
+            self.__jam += 24
+
+    def lamaLari(self):
+        return (self.__jam * 60) + self.__menit + (self.__detik / 60.0)
+
+
+class UjianTPB:
+    def __init__(self):
+        self.__nama = ""
+        self.__npm = ""
+        self.__mulai = Waktu()
+        self.__selesai = Waktu()
+
+    def setNama(self):
+        self.__nama = input("Masukkan Nama : ")
+
+    def setNpm(self):
+        self.__npm = input("Masukkan NPM : ")
+
+    def setMulai(self):
+        print("\n\n\tJam Mulai")
+        print("=======================")
+        self.__mulai.setJam()
+        self.__mulai.setMenit()
+        self.__mulai.setDetik()
+
+    def setSelesai(self):
+        print("\n\n\tJam Selesai")
+        print("=======================")
+        self.__selesai.setJam()
+        self.__selesai.setMenit()
+        self.__selesai.setDetik()
+        
+
+    def HurufMutu(self, lamaLari):
+        if 0 <= lamaLari < 7.5:
             return 'A'
-        elif lamaLari >= 7.5 and lamaLari < 12.5:
+        elif 7.5 <= lamaLari < 12.5:
             return 'B'
-        elif lamaLari >= 12.5 and lamaLari < 30:
+        elif 12.5 <= lamaLari < 30:
             return 'C'
-        else :
+        else:
             return 'D'
 
     def status(self, hurufMutu):
-        if hurufMutu == 'A' or hurufMutu == 'B' or hurufMutu == 'C' :
+        if hurufMutu in ['A', 'B', 'C']:
             return "Lulus"
-        else : 
+        else:
             return "Gagal"
-        
-    
-    def tampilan(self, selesai):
-        lama = self.lamaLari(selesai)
-        print("\n\nNama:", self.getNama())
-        print("NPM:", self.getNpm())
-        print("Huruf Mutu:", self.hurufMutu(lama))
-        print("Status:", self.status(self.hurufMutu(lama)))
-        print("Waktu Mulai:", self.getJam(), ":", self.getMenit(), ":", self.getDetik())
-        print("Waktu Selesai:", selesai.getJam(), ":", selesai.getMenit(), ":", selesai.getDetik())
-        print("Lama Lari:", lama, "Menit\n\n")
-        
-dataMhs = Waktu()
-selesai = Waktu()
 
-dataMhs.setNama()
-dataMhs.setNpm()
+    def tampilan(self):
+        selisih = Waktu()
+        selisih.selisihWaktu(self.__mulai, self.__selesai)
 
-print("\n\n\tJam Mulai")
-print("=======================")
+        lama_lari = selisih.lamaLari()
+        huruf_mutu = self.HurufMutu(lama_lari)
 
-dataMhs.setJam()
-dataMhs.setMenit()
-dataMhs.setDetik()
+        print("\n\n\tOUTPUT")
+        print("===========================")
+        print(f"\n\nNama : {self.__nama}")
+        print(f"NPM : {self.__npm}")
+        print(f"Huruf Mutu : {huruf_mutu}")
+        print(f"Status : {self.status(huruf_mutu)}")
+        print(f"Waktu Mulai : {self.__mulai.getJam()}:{self.__mulai.getMenit()}:{self.__mulai.getDetik()}")
+        print(f"Waktu Selesai : {self.__selesai.getJam()}:{self.__selesai.getMenit()}:{self.__selesai.getDetik()}")
+        print(f"Lama Lari : {lama_lari:.2f} Menit\n\n")
 
 
-print("\n\n\tJam Selesai")
-print("=======================")
+def Menu():
+    dataMhs = UjianTPB()
 
-selesai.setJam()
-selesai.setMenit()
-selesai.setDetik()
+    dataMhs.setNama()  # set nama
+    dataMhs.setNpm()  # set npm
+    dataMhs.setMulai() # set jam Mulai
+    dataMhs.setSelesai() # set jam selesai
+    dataMhs.tampilan() # tampilan output
 
-dataMhs.tampilan(selesai)
+
+if __name__ == "__main__":
+    Menu()
